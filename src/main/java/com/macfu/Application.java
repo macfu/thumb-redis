f9e6319c7f01b0b5c337d42f1849606a3fdf55ca
liming <v_fuliming@baidu.com>
2018-11-13 11:22:06 +0800

    "init program"

---
+++ src/main/java/com/macfu/Application.java
@@ -0,0 +1,26 @@
+package com.macfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @Author: liming
+ * @Date: 2018/11/13 11:16
+ * @Description:
+ */
+@SpringBootApplication
+@ServletComponentScan
+@MapperScan(basePackages = {"com.macfu.mapper"})
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.macfu")
+@EnableCaching
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
